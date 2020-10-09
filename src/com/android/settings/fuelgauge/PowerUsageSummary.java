eb6d952d6b87ba4128a93771c1cb6117b91a1ea7
IacobIonut01 <paulionut2003@gmail.com>
2020-10-09 08:22:53 +0300

    fuelgauge : Pause or cancel animation when fragment is destroyed prematurely when BatteryMeterView animation ends.
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.xyz>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -114,6 +114,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     boolean mNeedUpdateBatteryTip;
     @VisibleForTesting
     BatteryTipPreferenceController mBatteryTipPreferenceController;
+    @VisibleForTesting
+    ValueAnimator animator;
 
     @VisibleForTesting
     final ContentObserver mSettingsObserver = new ContentObserver(new Handler()) {
@@ -274,6 +276,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     @Override
     public void onPause() {
         getContentResolver().unregisterContentObserver(mSettingsObserver);
+        detachBatteryHeaderAnimationIfNecessary();
         super.onPause();
     }
 
@@ -427,7 +430,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mBatteryLevel = currentLevel;
         final int diff = Math.abs(prevLevel - currentLevel);
         if (diff != 0) {
-            final ValueAnimator animator = ValueAnimator.ofInt(prevLevel, currentLevel);
+            animator = ValueAnimator.ofInt(prevLevel, currentLevel);
             animator.setDuration(BATTERY_ANIMATION_DURATION_MS_PER_LEVEL * diff);
             animator.setInterpolator(AnimationUtils.loadInterpolator(getContext(),
                     android.R.interpolator.fast_out_slow_in));
@@ -443,6 +446,13 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
             animator.start();
         }
       }
+    }
+
+    @VisibleForTesting
+    void detachBatteryHeaderAnimationIfNecessary() {
+        if (animator.isRunning()) {
+            animator.pause();
+        }
     }
 
     @VisibleForTesting

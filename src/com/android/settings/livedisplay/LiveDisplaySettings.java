Havoc-OS/android_packages_apps_Settings

livedisplay: Don't show display mode and color tempertature on HWC2
Change-Id: Ib60e11bdb79a233c961d00c0968922264c042011

src/com/android/settings/livedisplay/LiveDisplaySettings.java
@@ -29,6 +29,7 @@ import android.support.v7.preference.PreferenceCategory;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.android.internal.app.ColorDisplayController;
 import com.android.internal.util.ArrayUtils;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -175,6 +176,10 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements I
         mLiveDisplay.setOnPreferenceChangeListener(this);
 
         mDisplayTemperature = (DisplayTemperature) findPreference(KEY_LIVE_DISPLAY_TEMPERATURE);
+        if (ColorDisplayController.isAvailable(getContext())) {
+            liveDisplayPrefs.removePreference(mLiveDisplay);
+            liveDisplayPrefs.removePreference(mDisplayTemperature);
+        }
 
         mColorProfile = (ListPreference) findPreference(KEY_LIVE_DISPLAY_COLOR_PROFILE);
         if (liveDisplayPrefs != null && mColorProfile != null

tompelitem/packages_apps_Settings-1

Signed-off-by: DennySPB <dennyspb@gmail.com>
Signed-off-by: DennySPb <dennyspb@gmail.com>

src/com/android/settings/livedisplay/LiveDisplaySettings.java
@@ -30,6 +30,7 @@ import androidx.preference.PreferenceCategory;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.android.internal.app.ColorDisplayController;
 import com.android.internal.util.ArrayUtils;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -176,6 +177,10 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements
         mLiveDisplay.setOnPreferenceChangeListener(this);
 
         mDisplayTemperature = (DisplayTemperature) findPreference(KEY_LIVE_DISPLAY_TEMPERATURE);
+        if (ColorDisplayManager.isNightDisplayAvailable(getContext())) {
+            liveDisplayPrefs.removePreference(mLiveDisplay);
+            liveDisplayPrefs.removePreference(mDisplayTemperature);
+        }
 
         mColorProfile = (ListPreference) findPreference(KEY_LIVE_DISPLAY_COLOR_PROFILE);
         if (liveDisplayPrefs != null && mColorProfile != null